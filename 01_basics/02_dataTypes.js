c833ce229cd8fc6261f45016411c6c7c13fd5d22
Shalini Biswal <shalinibiswal.codes@gmail.com>
2024-06-07 21:30:09 +0000

    knowing datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,51 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3
+    +
+     3) // code readability should be high
+
+     console.log("Shalini")
+
+
+     let name = "Manisha"
+     let age = 26
+     let isLoggedIn = true
+     let state;
+
+
+     //number => 2 to power 53
+     // bigint
+     // string => ""
+     // boolean => standalone value
+     // underdefined =>
+    // symbol => unique
+
+     
+     // object
+
+     console.log(typeof undefined); // undefined
+     console.log(typeof null); // object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
